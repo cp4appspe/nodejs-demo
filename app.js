9f641b1212979ca8c27d3671dd0317f0adabfdae
cp4appspe <cp4appspe@gmail.com>
2019-10-28 05:15:17 -0700

    Commit v3

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Kabanero Enterprise!");
+  res.send("Hello from Kabanero Enterprise version 0.1.x!");
 });
  
 module.exports.app = app;
